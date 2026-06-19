28b0f928a6f16f272e497c8209a791b0ff48ef4d
Paidipalli Keerthana <pkeerthana310107@email.com>
2026-06-19 12:14:45 +0530

    code

---
+++ server.js
@@ -0,0 +1,10 @@
+ const express = require("express");
+const app = express();
+const port = process.env.PORT || 6700;
+
+//to tell app, where to find the static files- html/css/js files are stored
+app.use(express.static("frontend"));
+
+app.listen(port,function(){
+    console.log("App is running on http://localhost:" + port);
+});
